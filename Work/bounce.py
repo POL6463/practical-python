047d6ea290c256a19dda51672fc279fe92752171
POL6463 <kevinkim0617kim@gmail.com>
2021-12-27 01:36:15 -0800

    12/27

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+
+ball_bounce = 0
+ball_height = 100
+
+while ball_bounce < 10:
+    ball_height *= 0.6
+    ball_bounce += 1
+    print(ball_bounce, round(ball_height, 4))
